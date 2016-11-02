21bb5edbbf9d0edb505dedf5c215af0b0ebb83e1
Robert Smol <robert.smol@stereoit.com>
2016-11-02 21:24:15 +0100

    piglatin: added description to rules and used revealing pattern

--- src/piglatin.js
+++ src/piglatin.js
@@ -1,8 +1,51 @@
-var PigLatin = {
-  piglatinize: function(text) {
-    return text;
+// PigLatin
+// ========
+//
+// Words that start with a consonant have their first letter moved to the end of the word and the letters “ay” added to the end.
+//  - Hello becomes Ellohay
+//
+// Words that start with a vowel have the letters “way” added to the end.
+//  - apple becomes appleway
+//
+// Words that end in “way” are not modified.
+//  - stairway stays as stairway
+//
+// Punctuation must remain in the same relative place from the end of the word.
+//  - can’t becomes antca’y
+//  - end. becomes endway.
+//
+// Hyphens are treated as two words
+//  - this-thing becomes histay-hinstay
+//
+// Capitalization must remain in the same place.
+//  - Beach becomes Eachbay
+//  - McCloud becomes CcLoudmay
+
+module.exports = (function(){
+
+  // vowels
+  var vowels = 'aeiouAEIOU';
+  // if (vowels.indexOf(letter.toLowerCase()) !== 1) {}
+
+  // consonants
+   var consonants = ' bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ';
+
+  // just stub implementation for now
+  function piglatinize(text) {
+    var tokens = text.split(' ')
+
+    // process each tokens
+    var latinized = tokens.map(function(token){
+      return token
+    })
+
+    return latinized.join(' ');
   }
-};
 
 
-module.exports = PigLatin;
+
+  return {
+      piglatinize: piglatinize
+  }
+
+})();
