6a1f612475670b9af8d91449d016d7d4b29d14f3
Robert Smol <robert.smol@stereoit.com>
2016-11-04 00:05:33 +0100

    Simplfied project structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  entry: 'src/app.js',
-  dest: 'src/public/js/bundle.js',
+  entry: 'app.js',
+  dest: './public/js/bundle.js',
   sourceMap: true,
   plugins: [
     commonjs({})
