6700c77ddafe8c59138c224872e6ecb979cc95c3
Robert Smol <robert.smol@stereoit.com>
2016-11-04 00:24:59 +0100

    Added functional version of translator

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 import Piglatin from './piglatin';
 
 console.log(Piglatin.piglatinize("Hello World!"))
+
+let plInput = document.getElementById("plInput")
+  , display = document.getElementById("translatedText")
+
+if (plInput && display) {
+  plInput.addEventListener("keyup", function(event){
+    const text = event.target.value
+    display.textContent = Piglatin.piglatinize(text)
+  });
+} else {
+  console.log("Need #plInput and #translatedText elements on the page!");
+}
